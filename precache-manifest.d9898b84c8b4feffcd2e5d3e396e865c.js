417343f4a3b7b29f60aad6029ba0c41968cb99e9
Supakoa <41123305+Supakoa@users.noreply.github.com>
2019-05-08 21:47:12 +0700

    Updates

---
+++ precache-manifest.d9898b84c8b4feffcd2e5d3e396e865c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c95ebfec0153967a9589d2822200157",
+    "url": "/e-learnSSRU-app/index.html"
+  },
+  {
+    "revision": "efe7724c5b00156a68ad",
+    "url": "/e-learnSSRU-app/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/e-learnSSRU-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "efe7724c5b00156a68ad",
+    "url": "/e-learnSSRU-app/static/js/main.05ca86f0.chunk.js"
+  },
+  {
+    "revision": "12ba7b398b240b283ef3",
+    "url": "/e-learnSSRU-app/static/js/runtime~main.d013a639.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/e-learnSSRU-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
