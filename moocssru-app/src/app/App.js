025b77e1f298fad12468c86ffab2c74c1705f950
Supakoa <41123305+Supakoa@users.noreply.github.com>
2019-05-09 00:50:07 +0700

    koa

--- moocssru-app/src/app/App.js
+++ moocssru-app/src/app/App.js
@@ -1,11 +1,56 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react';
+import {confirmAlert} from 'react-confirm-alert'; // Import
+import WOW from 'wowjs';
 
 export default class App extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  componentDidMount() {
+    new WOW
+      .WOW({live: false,})
+      .init();
+  }
+  submit = () => {
+    confirmAlert({
+      title: 'Confirm to submit',
+      message: 'Are you sure to do this.',
+      buttons: [
+        {
+          label: 'Yes',
+          onClick: () => alert('Click Yes')
+        }, {
+          label: 'No',
+          onClick: () => alert('Click No')
+        }
+      ]
+    });
+  };
+  render() {
+    return (
+      <div>
+        <div className='container'>
+          <button onClick={this.submit}>Confirm dialog</button>
+        </div>
+        {/* WOW */}
+        <div>
+          <h2>Ejemplos de WOW.js</h2>
+          <h3>1. data-wow-duration (5s):</h3>
+          <div className="wow rotateOutDownRight pink" data-wow-duration="5s">
+            <div/>
+          </div>
+          <h3>2. data-wow-delay (5s):</h3>
+          <div className="wow flipOutX blue" data-wow-delay="5s">
+            <div/>
+          </div>
+          <h3>3. data-wow-delay (5 iteraciones):</h3>
+          <div className="wow fadeInLeftBig green" data-wow-iteration="5">
+            <div/>
+          </div>
+          <h3>4. data-wow-offset (100px):</h3>
+          <div className="wow zoomIn orange" data-wow-offset="100">
+            <div/>
+          </div>
+        </div>
+
+      </div>
+    )
+  }
 }
--- moocssru-app/src/index.js
+++ moocssru-app/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
+import '../node_modules/animate.css/animate.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
