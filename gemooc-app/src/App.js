4322e5c51e116da650d8e08ef6ed7e41ff42c975
Supakoa <41123305+Supakoa@users.noreply.github.com>
2019-05-09 17:35:41 +0700

    koa

--- gemooc-app/src/App.js
+++ gemooc-app/src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import RouterApp from './router';
-function App() {
-  return (
-    <div >
-      <RouterApp/>
-    </div>
-  );
-}
+import React, { Component } from 'react'
+import AppSD from './student/AppSD';
+import AppTC from './teacher/AppTC';
+import AppAM from './admin/AppAM';
+import AppRG from './register/AppRG';
 
-export default App;
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <AppRG/>
+                <AppTC/>
+            </div>
+        )
+    }
+}
--- gemooc-app/src/router.js
+++ gemooc-app/src/router.js
@@ -1,79 +1,19 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React,{Component} from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import AppSD from './student/AppSD';
+import AppTC from './teacher/AppTC';
+import AppAM from './admin/AppAM';
+import AppRG from './register/AppRG';
 
-function RouterApp() {
-  return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
-        </ul>
-
-        <hr />
-
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/topics" component={Topics} />
-      </div>
-    </Router>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route exact path="/" component={AppRG}/>
+        <Route path="/about" component={AppSD}/>
+        <Route path="/topics" component={AppAM}/>
+        <Route path="/topics" component={AppTC}/>
+      </Router>
+    );
+  }
 }
-function Home() {
-    return (
-      <div>
-        <h2>Home</h2>
-      </div>
-    );
-  }
-  
-  function About() {
-    return (
-      <div>
-        <h2>About</h2>
-      </div>
-    );
-  }
-  
-  function Topics({ match }) {
-    return (
-      <div>
-        <h2>Topics</h2>
-        <ul>
-          <li>
-            <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-          </li>
-          <li>
-            <Link to={`${match.url}/components`}>Components</Link>
-          </li>
-          <li>
-            <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-          </li>
-        </ul>
-  
-        <Route path={`${match.path}/:topicId`} component={Topic} />
-        <Route
-          exact
-          path={match.path}
-          render={() => <h3>Please select a topic.</h3>}
-        />
-      </div>
-    );
-  }
-  
-  function Topic({ match }) {
-    return (
-      <div>
-        <h3>{match.params.topicId}</h3>
-      </div>
-    );
-  }
-  
-  export default RouterApp;
