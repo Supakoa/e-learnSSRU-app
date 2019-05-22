d0711a37526657220fe0092722ba93bbb7faf82b
Supakoa <41123305+Supakoa@users.noreply.github.com>
2019-05-22 16:25:42 +0700

    koa

--- GeMooc-app/resources/views/admin.blade.php
+++ GeMooc-app/resources/views/admin.blade.php
@@ -3,18 +3,7 @@
 @section('title', 'Home')
 
 @section('content')
-<table class="table">
-    <thead>
-        <tr>
-            <th>555555555555</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>
-                5
-            </td>
-        </tr>
-    </tbody>
-</table>
+<div class="text-center">
+    <h1>Home Page</h1>
+</div>
 @endsection
