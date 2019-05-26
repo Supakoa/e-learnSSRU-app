7a669bbe3aee5b593495ec021957bea354a5586a
S-Tawan <41279209+S-Tawan@users.noreply.github.com>
2019-05-26 20:27:07 +0700

    เพิ่มคอนเท้น

--- GeMooc-app/routes/web.php
+++ GeMooc-app/routes/web.php
@@ -16,12 +16,30 @@ Auth::routes();
 Route::get('/', 'HomeController@index')->name('dashboard.home');
 
 Route::get('/home', 'HomeController@index')->name('dashboard.home');
+Route::get('/test','test_sumernote@index');
+// Route::get('/up_image','test_sumernote@up_image');
+Route::post('ajaximage', function(){
+
+    $file = Request::file('file');
+    $destinationPath = public_path().'/uploads/';
+    $filenameWithExt = $file->getClientOriginalName();
+    $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+    $extension = $file->getClientOriginalExtension();
+    $fileNameToStore= $filename.'_'.time().'.'.$extension;
+    
+    $file->move($destinationPath, $fileNameToStore);
+    echo url('/uploads/'.$fileNameToStore);
+});
+
+// Route::get('test', function ($id) {
 
 // Route::get('/subject', '');
 
 Route::resource('/subject', 'subject_c');
 Route::resource('/course', 'course_c');
 Route::resource('/lesson', 'lesson_c');
+Route::resource('/content', 'content_c');
+
 
 
 
