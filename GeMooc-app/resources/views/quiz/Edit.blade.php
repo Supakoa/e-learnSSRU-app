30a03ac2620ebbe72f4aa0939d06183c328473eb
Supakoa <41123305+Supakoa@users.noreply.github.com>
2019-06-02 19:25:24 +0700

    koa

--- GeMooc-app/resources/views/quiz/Edit.blade.php
+++ GeMooc-app/resources/views/quiz/Edit.blade.php
@@ -6,6 +6,17 @@
         <a href="#" class="ce-arrow" style="font-size:25px" onclick="goBack()"><i class="fas fa-arrow-left"></i></a>
     </div>
     <h1 class="ce-name">Editor - Quiz</h1>
-    <div class="ce-container"></div>
+    <div class="ce-container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="create-quiz"></div>
+
+            </div>
+            <div class="col-md-6">
+                <div class="preview-quiz"></div>
+
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
