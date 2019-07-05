a2fd73b712edc7e9bb47fb4dacf9c25b3d5a5e66
S-Tawan <41279209+S-Tawan@users.noreply.github.com>
2019-07-05 12:32:13 +0700

    asd

--- GeMooc-app/database/factories/QuizFactory.php
+++ GeMooc-app/database/factories/QuizFactory.php
@@ -6,7 +6,9 @@ use App\quiz;
 use Faker\Generator as Faker;
 
 $factory->define(quiz::class, function (Faker $faker) {
+    $random =$faker->numberBetween($min = 1, $max =4);
+
     return [
-        //
+        'name' => $faker->province,
     ];
 });
