fd722546c790f91cdaca7ddb436faff0d8bc83d4
Supakoa <41123305+Supakoa@users.noreply.github.com>
2019-07-15 14:13:36 +0700

    koa

--- GeMooc-app/resources/views/admin-teach/webapp/content/subject/courses/Course.blade.php
+++ GeMooc-app/resources/views/admin-teach/webapp/content/subject/courses/Course.blade.php
@@ -13,6 +13,11 @@
         </div>
     </div>
     <div class="row">
+            <div class="your-class">
+                    <div>your content</div>
+                    <div>your content</div>
+                    <div>your content</div>
+                  </div>
         <div class="col-md-4">
             <div class="card-subject">
                 <div class="card-subject-header">
@@ -108,4 +113,46 @@
         </div>
     </div>
 </div>
+@endsection
+
+ิ
+@section('js')
+<script>
+$('.responsive').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+</script>
 @endsection
