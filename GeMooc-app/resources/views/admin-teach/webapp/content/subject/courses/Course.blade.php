8d82f8ec46280ce3dd72236147fa34ce0427410d
Supakoa <41123305+Supakoa@users.noreply.github.com>
2019-07-15 16:03:07 +0700

    koa

--- GeMooc-app/resources/views/admin-teach/webapp/content/subject/courses/Course.blade.php
+++ GeMooc-app/resources/views/admin-teach/webapp/content/subject/courses/Course.blade.php
@@ -75,72 +75,8 @@
                 </div>
             </div>
         </div>
-
         @endforeach
 
-
-        {{-- <div class="col-md-4">
-            <div class="card-subject">
-                <div class="card-subject-header">
-                    <div class="status-bar" style="background:red"></div>
-                    <img src="https://www.showbusinessweekly.com/wp-content/uploads/2016/12/Break-into-show-business-777x437.jpg"
-                        class="shadow" width="100%" height="100%">
-                </div>
-                <div class="card-subject-body">
-                    <div class="card-content-header">
-                        <p>subject name</p>
-                    </div>
-                    <div class="card-content-body">
-                        <ul class="list-unstyled">
-                            <li>
-                                บทเรียน 4 บท
-                            </li>
-                            <li>
-                                เปิดรับสมัคร 200/500 คน
-                            </li>
-                            <li>
-                                เปิดรับ 15 - 30 มิ.ย. 62
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="btn-subject">
-                        <button>ไปที่คอร์ส</button>
-                        <a><i class="fas fa-cog    "></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card-subject">
-                <div class="card-subject-header">
-                    <div class="status-bar" style="background: #6BB844;"></div>
-                    <img src="https://www.showbusinessweekly.com/wp-content/uploads/2016/12/Break-into-show-business-777x437.jpg"
-                        class="shadow" width="100%" height="100%">
-                </div>
-                <div class="card-subject-body">
-                    <div class="card-content-header">
-                        <p>subject name</p>
-                    </div>
-                    <div class="card-content-body">
-                        <ul class="list-unstyled">
-                            <li>
-                                บทเรียน 4 บท
-                            </li>
-                            <li>
-                                เปิดรับสมัคร 200/500 คน
-                            </li>
-                            <li>
-                                เปิดรับ 15 - 30 มิ.ย. 62
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="btn-subject">
-                        <button>ไปที่คอร์ส</button>
-                        <a><i class="fas fa-cog    "></i></a>
-                    </div>
-                </div>
-            </div>
-        </div> --}}
     </div>
 </div>
 
