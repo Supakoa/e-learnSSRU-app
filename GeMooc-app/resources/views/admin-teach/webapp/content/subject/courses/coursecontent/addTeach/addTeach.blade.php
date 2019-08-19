d9ce35b0f69dc14d89f0482221dfc6909ec3c339
Supakoa <41123305+Supakoa@users.noreply.github.com>
2019-08-19 10:16:03 +0700

    koa

--- GeMooc-app/resources/views/admin-teach/webapp/content/subject/courses/coursecontent/addTeach/addTeach.blade.php
+++ GeMooc-app/resources/views/admin-teach/webapp/content/subject/courses/coursecontent/addTeach/addTeach.blade.php
@@ -8,7 +8,7 @@
     <div class="card-body">
         <div class="row"
         style="border-bottom: 2px solid gray;
-                width:98%;
+                width:99%;
                 margin:auto;">
             <div class="col-md-12">
                 <h3>เพิ่มผู้สอนในคอร์ส</h3>
@@ -16,15 +16,15 @@
         </div>
         <div class="row">
             <div class="col-md-6">
-                <p class="pl-3 pt-3">ตารางผู้สอน</p>
+                <p class="pl-2 pt-3">ตารางผู้สอน</p>
             </div>
-            <div class="col-md-2 offset-md-4">
-
+            <div class="col-md-2 offset-md-4 pl-3 pt-3">
+                <button class=""><i class="fa fa-user-plus" aria-hidden="true"></i></button>
             </div>
         </div>
         <div class="row">
             <div class="col-md-12">
-                <div class="container table-responsive">
+                <div class="table-responsive">
                     <table class="display table table-hover table-borderless" id="addTeach">
                         <thead class="text-center">
                             <tr>
@@ -43,10 +43,37 @@
                                 <td>
 
                                 </td>
-                                <td>
+                                <td class="text-center">
                                     <button><i class="fas fa-pencil-alt    "></i></button>
                                     <button><i class="fa fa-trash" aria-hidden="true"></i></button>
                                 </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <p class="pl-2 pt-3">ตารางผู้เรียน</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="table-responsive">
+                    <table class="display table table-hover table-borderless" id="tableAddteach">
+                        <thead class="text-center">
+                            <tr>
+                                <th scope="col">ลำดับ</th>
+                                <th>ชื่อ - นามสกุล</th>
+                                <th>เริ่มเรียน</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>1</th>
+                                <td></td>
+                                <td></td>
                             </tr>
                         </tbody>
                     </table>
@@ -61,6 +88,7 @@
     <script>
         $(document).ready(function () {
             $('#addTeach').DataTable();
+            $('#tableAddteach').DataTable();
         });
     </script>
 @endsection
