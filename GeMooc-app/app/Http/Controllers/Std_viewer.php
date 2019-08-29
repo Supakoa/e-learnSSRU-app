96438be3179f83b08fe7133dc1418fdb77bcb44d
MessageAlert <41282062+MessageAlert@users.noreply.github.com>
2019-08-30 02:35:56 +0700

    to day phase 1[edit]
    แก้ไขปัญหา record ใหม่ที่ยังไม่มีตรง controller std_view

--- GeMooc-app/app/Http/Controllers/Std_viewer.php
+++ GeMooc-app/app/Http/Controllers/Std_viewer.php
@@ -85,9 +85,24 @@ class Std_viewer extends Controller
 
             $issetRecord = ($record != null);
 
-            $recordContentId = $record->content_id;
-            $recordRecord = $record->record;
-            $recordPercent = $record->percent;
+            if(!$issetRecord){
+
+                // create new
+                $newRecord = new record();
+                $newRecord->content_id = $contentId;
+                $newRecord->user_id = auth()->user()->id;
+                $newRecord->record = json_encode(array());
+                $newRecord->percent = '0';
+                $newRecord->save();
+
+                $recordContentId = $newRecord->content_id;
+                $recordRecord = $newRecord->record;
+                $recordPercent = $newRecord->percent;
+            }else{
+                $recordContentId = $record->content_id;
+                $recordRecord = $record->record;
+                $recordPercent = $record->percent;
+            }
 
             $recordO = array(
                 'contentId' => $recordContentId,
