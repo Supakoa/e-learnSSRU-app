83047662aca09385d5ed89e6948140c465bad2f9
S-Tawan <41279209+S-Tawan@users.noreply.github.com>
2019-08-30 14:27:23 +0700

    FAQ MANUAL

--- GeMooc-app/resources/views/pagestudent/FAQ/FAQ.blade.php
+++ GeMooc-app/resources/views/pagestudent/FAQ/FAQ.blade.php
@@ -11,15 +11,15 @@
     <div class="container">
             <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner">
-                      <div class="carousel-item active">
-                        <img src="..." class="d-block w-100" alt="...">
-                      </div>
-                      <div class="carousel-item">
-                        <img src="..." class="d-block w-100" alt="...">
-                      </div>
-                      <div class="carousel-item">
-                        <img src="..." class="d-block w-100" alt="...">
-                      </div>
+                            @foreach ($faqs as$key=>$faq)
+                            <div class="carousel-item {{$key==0 ? 'active' : ''}}">
+                                    <img src="{{url(''.$faq->image)}}" class="d-block w-100" alt="...">
+                          <div class="carousel-caption d-none d-md-block">
+                            {{-- <h5>First slide label</h5>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> --}}
+                          </div>
+                        </div>
+                        @endforeach
                     </div>
                     <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
                       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
