91fb96924bba2c29aa0788b4a7d2fee7741219d6
Supakoa <41123305+Supakoa@users.noreply.github.com>
2019-09-28 01:27:03 +0700

    koa

--- GeMooc-app/resources/views/pagestudent/FAQ/FAQ.blade.php
+++ GeMooc-app/resources/views/pagestudent/FAQ/FAQ.blade.php
@@ -8,27 +8,23 @@
 @endpush
 
 @section('mainContent')
-    <div class="container-fluid">
-            <div id="carouselExampleControls m-auto" class="carousel slide" data-ride="carousel">
-                    <div class="carousel-inner">
-                            @foreach ($faqs as$key=>$faq)
-                            <div class="carousel-item {{$key==0 ? 'active' : ''}}">
-                                    <img src="{{url(''.$faq->image)}}" class="d-block w-100 rounded" alt="...">
-                          <div class="carousel-caption d-none d-md-block">
-                            {{-- <h5>First slide label</h5>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> --}}
-                          </div>
-                        </div>
-                        @endforeach
-                    </div>
-                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                      <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                      <span class="sr-only">Next</span>
-                    </a>
-                  </div>
+<div class="container-fluid">
+    <div id="carouselExampleControls m-auto" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            @foreach ($faqs as $key => $faq)
+            <div class="carousel-item {{$key==0 ? 'active' : ''}}">
+                <a href="#"><img src="{{url(''.$faq->image)}}" class="d-block w-100 rounded" alt="..."></a>
+            </div>
+            @endforeach
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
+</div>
 @endsection
